77c0a2a9bb03c5a786743bc73f6cfa300e6a6b2d
shuckin2017 <shuckin@mail.ru>
2020-04-02 15:14:48 +0300

    install sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,8 @@
-let gulp = require('gulp')
+let gulp = require('gulp'), 
+    sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+    return gulp.src('src/sass/**/*.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('src/css')); 
+});
