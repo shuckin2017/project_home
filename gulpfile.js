a4aa4ecf584edf8ece9dac1052a626c218465ced
shuckin2017 <shuckin@mail.ru>
2020-04-02 15:59:40 +0300

    install browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,25 @@
 let gulp = require('gulp'), 
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create();
 
 gulp.task('sass', function(){
     return gulp.src('src/sass/**/*.sass')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('src/css')); 
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(gulp.dest('src/css'))
+        .pipe(browserSync.reload({stream: true}))
 });
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "src/"
+        }
+    });
+});
+
+
+gulp.task('watch', function(){
+    gulp.watch('src/sass/**/*.sass', gulp.parallel('sass'))
+  });
+
+gulp.task('default', gulp.parallel('browser-sync', 'watch'))
